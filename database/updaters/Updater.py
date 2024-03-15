b5821918e3beb9d22e70d400023a253b649a3b31
SiwooBae <cymetric1@gmail.com>
2024-03-14 22:24:50 -0500

    Revert "Delete database/updaters/Updater.py"
    This reverts commit fbef333cc2c04980a7e9aef1a5bd40956cda442a.

---
+++ database/updaters/Updater.py
@@ -0,0 +1,90 @@
+from urllib.request import urlopen
+import certifi
+import json
+from io import StringIO
+import pandas as pd
+
+APIKEY = '05ad7c34febe7abdd32257d3ece2b148'
+
+
+def get_jsonparsed_data(url):
+    response = urlopen(url + "&apikey=" + APIKEY, cafile=certifi.where())
+    data = response.read().decode("utf-8")
+    return json.loads(data)
+
+# This class is responsible for downloading the newest data from FinancialModelingPrep API to my database.
+class Updater:
+    def __init__(self):
+        self.dropped_features = ['link', 'finalLink']
+        self.stock_exchange = ['NYSE', 'NASDAQ']
+        self.url_root = "https://financialmodelingprep.com/api"
+
+    def get_balance_sheet(self, symbol, period='quarter'):
+        data = get_jsonparsed_data(self.url_root + "/v3/balance-sheet-statement/" + symbol + "?" + "period=" + period
+                                   )
+        return pd.read_json(data)
+
+    def get_income_statement(self, symbol, period='quarter'):
+        data = get_jsonparsed_data(self.url_root + "/v3/income-statement/" + symbol + "?" + "period=" + period
+                                   )
+        return pd.read_json(data)
+
+    def get_cash_flow_statement(self, symbol, period='quarter'):
+        data = get_jsonparsed_data(self.url_root + "/v3/cash-flow-statement/" + symbol + "?" + "period=" + period
+                                   )
+        return pd.read_json(data)
+
+    def get_balance_sheet_bulk(self, year, period='quarter'):
+        url = self.url_root + "/v4/balance-sheet-statement-bulk?year=" + str(year) + "&" + "period=" + period
+        response = urlopen(url + "&apikey=" + APIKEY, cafile=certifi.where())
+        data = StringIO(response.read().decode("utf-8"))
+        return pd.read_csv(data)
+
+    def get_income_statement_bulk(self, year, period='quarter'):
+        url = self.url_root + "/v4/income-statement-bulk?year=" + str(year) + "&" + "period=" + period
+        response = urlopen(url + "&apikey=" + APIKEY, cafile=certifi.where())
+        data = StringIO(response.read().decode("utf-8"))
+        return pd.read_csv(data)
+
+    def get_cash_flow_statement_bulk(self, year, period='quarter'):
+        url = self.url_root + "/v4/cash-flow-statement-bulk?year=" + str(year) + "&" + "period=" + period
+        response = urlopen(url + "&apikey=" + APIKEY, cafile=certifi.where())
+        data = StringIO(response.read().decode("utf-8"))
+        return pd.read_csv(data)
+
+    def get_all_profiles(self):
+        return get_jsonparsed_data(self.url_root + "/v4/profile/all?")
+
+    def get_all_symbols_with_financial_statements(self):
+        return get_jsonparsed_data(self.url_root + "/v3/financial-statement-symbol-lists?")
+
+    def get_symbol_changes(self):
+        response = urlopen(self.url_root + "/v4/symbol_change?apikey=" + APIKEY, cafile=certifi.where())
+        data = response.read().decode("utf-8")
+        return json.loads(data)
+
+    def get_stock_news(self, page: int):
+        return get_jsonparsed_data(self.url_root + f"/v3/stock_news?page={page}")
+
+    def get_general_news(self, page: int):
+        return get_jsonparsed_data(self.url_root + f"/v4/general_news?page={page}")
+
+    def get_delisted_symbols(self):
+        return get_jsonparsed_data(self.url_root + "/v4/delisted-companies?")
+
+    def get_stock_peers(self, symbol):
+        return get_jsonparsed_data(self.url_root + "/v4/stock_peers?symbol=" + symbol)
+
+    def get_daily_price(self, symbol, date_from, date_to):
+        return get_jsonparsed_data(
+            self.url_root + "/v3/historical-chart/1day/" + symbol + "?from="+str(date_from)+"&to="+str(date_to))
+
+    def get_macro_indicator(self, indicator):
+        return get_jsonparsed_data(
+            self.url_root + "/v4/economic?name=" + indicator)
+
+    def get_treasury(self, date_from, date_to):
+        return get_jsonparsed_data(self.url_root + f"/v4/treasury?from={str(date_from)}&to={str(date_to)}")
+
+
+
